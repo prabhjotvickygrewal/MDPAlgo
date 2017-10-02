cb2ae37ee49fd279a83c85eb2b190fb6612b03c7
kkkkkwai <32378700+kkkkkwai@users.noreply.github.com>
2017-10-02 23:02:28 +0800

    descriptor modified

--- MDPAlgo/src/map/Descriptor.java
+++ MDPAlgo/src/map/Descriptor.java
@@ -1,6 +1,13 @@
 package map;
 import java.util.*;
+
+import map.Map;
+import map.Vector;
+
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 public class Descriptor {
 
 	public static PointState[][] getStatesFromHex(String hex1, String hex2){
@@ -23,24 +30,29 @@ public class Descriptor {
 			if(binary1.charAt(i)=='0')
 				states[i/Map.MAX_Y][i%Map.MAX_Y]=PointState.Unknown;
 		
-		for(int i = 0; i < hex2.length(); i++){           //convert char one by one
-	        binFragment = Integer.toBinaryString(Integer.parseInt(""+hex1.charAt(i),16));
-	        while(binFragment.length() < 4){
-	            binFragment = "0" + binFragment;
-	        }
-	        binary2 += binFragment;
-	    }
-		int cur=0;
-		for(int i=0;i<Map.MAX_X;i++)
-			for(int j=0;j<Map.MAX_Y;j++){
-				if(states[i][j]!=PointState.Unknown){
-					if(binary2.charAt(cur)=='0')
-						states[i][j]=PointState.IsFree;
-					else
-						states[i][j]=PointState.Obstacle;
-					cur++;
+		if(hex2!=null)
+			for(int i = 0; i < hex2.length(); i++){           //convert char one by one
+		        binFragment = Integer.toBinaryString(Integer.parseInt(""+hex2.charAt(i),16));
+		        while(binFragment.length() < 4){
+		            binFragment = "0" + binFragment;
+		        }
+		        binary2 += binFragment;
+		    }
+			int cur=0;
+			for(int i=0;i<Map.MAX_X;i++)
+				for(int j=0;j<Map.MAX_Y;j++){
+					if(states[i][j]!=PointState.Unknown){
+						if(binary2.charAt(cur)=='0')
+							states[i][j]=PointState.IsFree;
+						else
+							states[i][j]=PointState.Obstacle;
+						cur++;
+					}
 				}
-			}
+		
+		Map m=new Map();
+		m.updatePointMap(states);
+		m.printMap();
 		return states;
 	}
 	
@@ -60,7 +72,7 @@ public class Descriptor {
 			String binFragment=binary1.substring(i, i+4);
 			hex1=hex1+Integer.toString(Integer.parseInt(binFragment, 2), 16);
 		}
-		
+//		System.out.println(hex1);
 		return hex1;
 	}
 	public static String getSecondStringFromStates(PointState[][] states){
@@ -70,34 +82,31 @@ public class Descriptor {
 		for(int i=0;i<Map.MAX_X;i++)
 			for(int j=0;j<Map.MAX_Y;j++){
 				if(states[i][j]==PointState.IsFree)
-					binary2 +="0";
+					binary2 =binary2+"0";
 				else if(states[i][j]==PointState.Obstacle)
-					binary2 += "1";
+					binary2 =binary2+ "1";
 			}
 		int cur=0;
-		for(int i=0;i<binary2.length()/4+1;i++){
-			String binFragment="";
-			while(binFragment.length()<4){
-				if(cur<binary2.length())
-					binFragment = binFragment+binary2.charAt(cur);
-				else
-					binFragment = binFragment+"0";
-				cur++;
-			}
-			hex2=hex2+Integer.toString(Integer.parseInt(binFragment, 2), 16);
+		while(binary2.length()%4!=0)
+			binary2 = binary2+"0";
+		for(int i=0;i<binary2.length()/4;i++){
+			String binFragment=binary2.substring(i*4, i*4+4);
+			hex2 = hex2+Integer.toString(Integer.parseInt(binFragment, 2), 16);
 		}
-		
+//		System.out.println(hex2);
 		return hex2;
 	}
 
-	public PointState[][] getStatesFromFile(String fileName){
+	public static PointState[][] getStatesFromFile(String fileName){
             String filePath=new File("").getAbsolutePath();
             try{
                 BufferedReader br=new BufferedReader(new FileReader(
-                    filePath+File.separator+"src"+File.separator+"map"+File.separator+"test"+fileName));
+                    filePath+File.separator+"src"+File.separator+"map"+File.separator+"test"+File.separator+fileName));
                 String line1, line2;
                 line1=br.readLine();
+                System.out.println(line1);
                 line2=br.readLine();
+                System.out.println(line2);
                 if(line1!=null && line2!=null)
                     return getStatesFromHex(line1,line2);
                 return null;
@@ -107,19 +116,49 @@ public class Descriptor {
                 return null;
             }
         }
-        public void writeFileFromStates(String fileName, PointState[][] states){
+        public static void writeFileFromStates(String fileName, PointState[][] states){
             String hex1=getFirstStringFromStates(states);
             String hex2=getSecondStringFromStates(states);
             String filePath=new File("").getAbsolutePath();
-            try{
-                File file=new File(filePath+File.separator+"src"+File.separator+"map"+File.separator+"test"+fileName);
-                BufferedWriter br=new BufferedWriter(new FileWriter(file));
-                br.write(hex1);
-                br.newLine();
-                br.write(hex2);
-            }
-            catch(IOException e){
-                e.printStackTrace();
-            }
+            System.out.println(filePath+File.separator+"src"+File.separator+"map"+File.separator+"result"+File.separator+fileName);
+            if(hex1.length()!=0 && hex2.length()!=0)
+	            try{
+	//                File file=new File(filePath+File.separator+"src"+File.separator+"map"+File.separator+"result"+File.separator+fileName);
+	                Files.write(Paths.get(filePath+File.separator+"src"+File.separator+"map"+File.separator+"result"+File.separator+fileName), 
+	                		String.format("%s%n%s", hex1, hex2).getBytes(),
+	                		StandardOpenOption.CREATE);
+	//                BufferedWriter br=new BufferedWriter(new FileWriter(file, true));
+	//                br.write(hex1);
+	//                System.out.print(hex1);
+	//                br.newLine();
+	//                br.write(hex2);
+	//                System.out.print(hex2);
+	
+	            }
+	            catch(IOException e){
+	                e.printStackTrace();
+	            }
         }
+        
+//        public static void main(String[] args){
+//            LinkedList<Vector> obstacle=new LinkedList<>();
+//            obstacle.add(new Vector(15,7));
+//            obstacle.add(new Vector(14,7));
+//            obstacle.add(new Vector(13,7));
+//            obstacle.add(new Vector(16,7));
+//            obstacle.add(new Vector(16,8));
+//            obstacle.add(new Vector(16,9));
+//            obstacle.add(new Vector(16,10));
+//            obstacle.add(new Vector(15,10));
+//            obstacle.add(new Vector(14,10));
+//            obstacle.add(new Vector(13,10));
+//            Map m=new Map(obstacle);
+//            
+//        	PointState[][] states=new PointState[Map.MAX_X][Map.MAX_Y];
+//        	for(int i=0;i<Map.MAX_X;i++)
+//        		for(int j=0;j<Map.MAX_Y;j++)
+//        			states[i][j]=m.getPointStateAt(new Vector(i,j));
+//        	
+//        	Descriptor.writeFileFromStates("test.txt", states);
+//        }
 }
